62cc68c4f67c8d5e0cbf9095ef747cbd93b2b0af
Ian Carthey <PimpDaddyE@Ians-MacBook-Pro-2.local>
2018-03-09 16:52:52 -0600

    input functions adding to dom

--- client.js
+++ client.js
@@ -13,3 +13,41 @@ function readyNow(){
 function addEventHandlers(){
   $('#submit').on('click', addInputs);
 }
+
+//function to addInputs
+function addInputs(){
+  // addingInfoToTable();
+  // newInputsEditors();
+  //input variables
+  let userNameInput = $('#firstNameInput').val();
+  let userLastName = $('#lastNameInput').val();
+  let userIdNumber = $('#idNumInput').val();
+  let userTitle = $('#titleInput').val();
+  let userSalary = $('#salaryInput').val();
+
+  $('#tableBody').append('<tr><td>' + userNameInput + '</td>' + '<td>'
+  + userLastName + '</td>' + '<td>' + userIdNumber + '</td>' + '<td>'
+  + userTitle + '</td>' + '<td>' + userSalary + '</td></tr>');
+  newInputsEditors();
+}
+
+
+function newInputsEditors(){
+  $('#tableBody').addClass('addedTableInputs');
+}
+
+
+// //input variables
+// let userNameInput = $('#firstNameInput').val();
+// let userLastName = $('#lastNameInput').val();
+// let userIdNumber = $('#idNumInput').val();
+// let userTitle = $('#titleInput').val();
+// let userSalary = $('#salaryInput').val();
+
+//function to add inputs to table
+// function addingInfoToTable(){
+//   $('#tableBody').append('<tr><td>' + userNameInput + '</td>' + '<td>'
+//   + userLastName + '</td>' + '<td>' + userIdNumber + '</td>' + '<td>'
+//   + userTitle + '</td>' + '<td>' + userSalary + '</td></tr>');
+//   newInputsEditors();
+// }
