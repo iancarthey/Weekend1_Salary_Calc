020d176aeb2bcc7646947a3436ec6bec16dcbcf4
Ian Carthey <PimpDaddyE@Ians-MacBook-Pro-2.local>
2018-03-10 18:11:42 -0600

    finished total cost, now start clear fields

--- client.js
+++ client.js
@@ -1,6 +1,9 @@
 /* jshint esversion: 6 */
 console.log('js');
 //checking that js is linked
+
+
+
 $(document).ready(readyNow);
 
 function readyNow(){
@@ -19,21 +22,25 @@ function addInputs(){
   // addingInfoToTable();
   // newInputsEditors();
   //input variables
-  let userNameInput = $('#firstNameInput').val();
-  let userLastName = $('#lastNameInput').val();
-  let userIdNumber = $('#idNumInput').val();
+  let nameInput = $('#firstNameInput').val();
+  let lastName = $('#lastNameInput').val();
+  let idNumber = $('#idNumInput').val();
   let userTitle = $('#titleInput').val();
-  let userSalary = $('#salaryInput').val();
+  let userSalary = Number($('#salaryInput').val());
+  console.log(userSalary);
 
-  $('#tableBody').append('<tr class="newFormat"><td>' + userNameInput + '</td>' + '<td>'
-  + userLastName + '</td>' + '<td>' + userIdNumber + '</td>' + '<td>'
+  $('#tableBody').append('<tr class="newFormat"><td>' + nameInput + '</td>' + '<td>'
+  + lastName + '</td>' + '<td>' + idNumber + '</td>' + '<td>'
   + userTitle + '</td>' + '<td>' + userSalary + '</td></tr>');
   newInputsEditors();
 
-//calculating monthly and posting to dom
-  $('#monthlyCost').empty();
+//for calculating total monthly cost
+  let newEmployee = new Employee(nameInput, lastName, idNumber, userTitle, userSalary);
+  employeeArray.push(newEmployee);
 
-  let inputMonthlyCost = userSalary / 12;
+  initialMonthlySalary (userSalary);
+
+
 
 }
 
@@ -42,21 +49,38 @@ function newInputsEditors(){
   $('#tableBody').addClass('addedTableInputs');
 }
 
+class Employee {
+  constructor(firstNameIn, lastNameIn, idIn, titleIn, salaryIn){
+    this.firstName = firstNameIn;
+    this.LastName = lastNameIn;
+    this.id = idIn;
+    this.title = titleIn;
+    this.salary = salaryIn;
+  }
+}
+
+let employeeArray = [];
+
+function initialMonthlySalary( salary ){
+  let monthlySalary = 0;
+  for(let employee of employeeArray){
+    monthlySalary += salary;
+  }
+  finalMonthlyCost( monthlySalary );
+}
+
+function finalMonthlyCost( monthlySalary ){
+  let monthlyCost = monthlySalary / 12;
+  $('#monthlyFace').empty();
+  $('#monthlyFace').append("Total Monthly: " + Number(monthlyCost).toFixed(2));
+  if ( monthlyCost > 20000){
+    $('#monthlyFace').empty();
+    $('#monthlyFace').append("Total Monthly: " + Number(monthlyCost).toFixed(2));
+    $('#monthlyFace').css('background-color', 'red');
+  }
+}
+
 // function calculatingMonthlyCost(){
 //
 //   $('#monthlyCost').append('Total Monthly: ' + inputMonthlyCost);
-// }
-// //input variables
-// let userNameInput = $('#firstNameInput').val();
-// let userLastName = $('#lastNameInput').val();
-// let userIdNumber = $('#idNumInput').val();
-// let userTitle = $('#titleInput').val();
-// let userSalary = $('#salaryInput').val();
-
-//function to add inputs to table
-// function addingInfoToTable(){
-//   $('#tableBody').append('<tr><td>' + userNameInput + '</td>' + '<td>'
-//   + userLastName + '</td>' + '<td>' + userIdNumber + '</td>' + '<td>'
-//   + userTitle + '</td>' + '<td>' + userSalary + '</td></tr>');
-//   newInputsEditors();
 // }
